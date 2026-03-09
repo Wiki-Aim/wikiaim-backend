ff04f3f9663f2e7cd1ec603840d3e3188e24f193
erwan <erwangodelle@gmail.com>
2026-03-09 16:35:43 +0100

    fix: .fr

--- src/main/java/com/wikiaim/backend/auth/DiscordApiClient.java
+++ src/main/java/com/wikiaim/backend/auth/DiscordApiClient.java
@@ -5,7 +5,7 @@ import io.micronaut.http.annotation.Header;
 import io.micronaut.http.client.annotation.Client;
 
 @Client("discord")
-@Header(name = "User-Agent", value = "WikiAim Backend (https://wikiaim.com, 0.1)")
+@Header(name = "User-Agent", value = "WikiAim Backend (https://wikiaim.fr, 0.1)")
 @Header(name = "Accept", value = "application/json")
 public interface DiscordApiClient {
 
